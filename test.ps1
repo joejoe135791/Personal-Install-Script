b24b9cbcf34d5356a742e24e425ae798d83d51e0
Joejoe <82959624+joejoe135791@users.noreply.github.com>
2024-04-21 01:58:15 -0700

    e

--- test.ps1
+++ test.ps1
@@ -1,11 +1,10 @@
-# start of script "housekeeping"
-$Tab = [char]9 #Needed for A tab character
 # listing packages
-Write-Output "The following packages will be installed"
+Write-Output "The following packages will be uninstalled"
 Write-Output "$Tab epicgameslauncher,"
 Write-Output "$Tab steam," 
 Write-Output "$Tab googlechrome," 
 Write-Output "$Tab operagx"
+Write-warning "THIS ACTION IS IRREVERSIBLE"
 $continue = Read-Host -Prompt "Continue? [y/n]"
 
 if ( $continue -eq 'n' ) { 
