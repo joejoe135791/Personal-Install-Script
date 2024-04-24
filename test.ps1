4b821a0382eb3fbdc312ef3cb9cc8aff0eb53958
Joejoe <82959624+joejoe135791@users.noreply.github.com>
2024-04-24 11:02:40 -0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,9 @@
+Write-Warning "This script is used for testing small portions of code and should never be run by a normal user"
+Write-Warning "This code may be completely untested and should only be run as a test"
+Write-Warning "By running this code, you accept all risk" -WarningAction Inquire 
+# DO NOT REMOVE ANYTHING ABOVE THIS LINE UNDER ANY CIRCUMSTANCE
+
+#place code under this line
 # listing packages
 Write-Output "The following packages will be uninstalled"
 Write-Output "$Tab epicgameslauncher,"
